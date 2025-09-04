c569105cfa5ad82ca6262145aacbcced2be60980
laveshgupta94 <guptalavesh925@gmmail.com>
2025-09-04 21:57:30 +0530

    updated

--- square_fill.py
+++ square_fill.py
@@ -1,5 +1,7 @@
-n=int(input())
-for i in range(1,n+1):
-    for j in range(1,n+1):
+
+r=int(input())
+c=int(input())
+for i in range(1,r+1):
+    for j in range(1,c+1):
         print("*",end=" ")
     print()
